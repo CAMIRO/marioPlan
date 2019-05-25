9e4c46a6dd0beee18edd0f3bd374ea7f877fa0f2
camiro <juancamilosc@hotmail.com>
2019-05-25 10:39:08 -0500

    pasing data into components with redux

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
 import Notifications from "./Notifications";
 import ProjectList from "../projects/ProjectList";
+//6. connect with our store
+import { connect } from "react-redux"; // glue redux with react
 
 class Dashboard extends Component {
   render() {
+    //console.log(this.props);
+    const { projects } = this.props;
     return (
       <div className="dashboard container">
         <div className="row">
           <div className="col s12 m6">
-            <ProjectList />
+            <ProjectList projects={projects} />
           </div>
           <div className="col s12 m5 offset-m1">
             <Notifications />
@@ -18,4 +22,11 @@ class Dashboard extends Component {
     );
   }
 }
-export default Dashboard;
+// 8. we need to map our state from the store to the props of this component. We do that by creating ths function
+const mapStateToProps = state => {
+  //9. return an Object that is going to represent which properties are attached to the props so we can access then inside this component
+  return {
+    projects: state.project.projects
+  };
+};
+export default connect(mapStateToProps)(Dashboard); // 7 connect is a function which return a HOC to taking the dashboard
--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ProjectSummary from "./ProjectSummary";
 
-const ProjectList = () => {
+const ProjectList = ({ projects }) => {
   return (
     <div className="project-list section">
-      <ProjectSummary />
-      <ProjectSummary />
-      <ProjectSummary />
-      <ProjectSummary />
+      {/*  { projects && projects.map(project =>{})} this way you validate if is projects inside the store, id dont...dont do the map loop   */}
+      {projects &&
+        projects.map(project => {
+          return <ProjectSummary project={project} key={project.id} />;
+        })}
     </div>
   );
 };
